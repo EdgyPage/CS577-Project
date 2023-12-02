4df36eebb1f5acd3c39f769ce49c847bd85be274
EdgyPage <myfirstbattle@gmail.com>
2023-12-01 19:55:57 -0600

    MMadee clloosestWordsDisstance to prove  gendered ddifference change

--- Results/analyze.py
+++ Results/analyze.py
@@ -45,6 +45,27 @@ def farthestWordsNorm(embedDict: dict , arr: np.ndarray, n: int = 5):
             bottomWords = sorted(bottomWords, key= lambda x: x[1])
     return bottomWords
 
+def closestWordsDistances(spotlightWords: [str], embedGDict: dict , embedNDict: dict , n: int = 5):
+    
+    distancesG = []
+    distancesN = []
+    for word in spotlightWords:
+        if word in embedGDict.keys() and word in embedNDict.keys():
+            topGWords = closestWordsNorm(embedGDict, embedGDict[word], n)
+            topNWords = closestWordsNorm(embedNDict, embedNDict[word], n)
+            for i in range(n):
+                if topGWords[i][0] in embedGDict.keys() and topGWords[i][0] in embedNDict.keys():
+                    distancesG.append(embedCos(embedGDict[topGWords[i][0]], embedGDict[word]))
+                    #print(distancesG.append(embedCos(embedGDict[topGWords[i][0]], embedGDict[word])))
+                    distancesN.append(embedCos(embedNDict[topGWords[i][0]], embedGDict[word]))
+    if sum(distancesG) == 0:
+        percentDiff = 0
+    else:
+        percentDiff = round(sum(distancesN) /sum(distancesG), 3)
+
+    return percentDiff
+
+
 if False:
     print(f'norm: {norm(cat - dog)}')
     cosine = np.dot(cat, dog) / (norm(cat) * norm(dog))
@@ -124,7 +145,7 @@ def plotter(pairs:[(str, str)], spotlightWords: [str], neutralEmbeds: dict, gend
             indices.append(spotlightWords.index(empWord))
             """
         
-            
+
 
             x_values = np.linspace(min(min(genderEmbeds[g1]), min(neutralEmbeds[g1]), min(genderEmbeds[g2]), min(neutralEmbeds[g2])),\
                                 max(max(genderEmbeds[g1]), max(neutralEmbeds[g1]), max(genderEmbeds[g2]), max(neutralEmbeds[g2])), 1000)
